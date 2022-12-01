2f5074df29e2fa766d8ac57ff9c884a644cbe9a3
Juanita Buriticá Rodríguez <juanita.buriticaro@autonoma.edu.co>
2022-12-01 16:40:47 -0500

    paso-a-paso-3

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,83 @@
 import React from 'react';
-import { Text,Modal,Button,View } from 'react-native';
+import {
+  Text,
+  Modal,
+  View,
+  StyleSheet,
+  View,
+  SafeAreaView,
+  ScrollView,
+  TextInput,
+} from 'react-native';
 
 export const From = ({modalVisible, newUser}) => {
-    
-    return (
-        <Modal animationType='slide' visible={modalVisible}>
-            <View>
-            <Text>Formulario de registro</Text>
-            <Button title='Aceptar' onPress={newUser}/>
-            </View>
-        </Modal>
-
-    );
-
+  return (
+    <Modal animationType="slide" visible={modalVisible}>
+      <SafeAreaView style={StyleSheet.content}>
+        <ScrollView>
+          <Text style={styles.title}>
+            Nuevo{''}
+            <Text style={styles.boldTitle}>Usuario</Text>
+          </Text>
+          <View style={styles - text_input}>
+            <Text style={styles.label}>Nombre</Text>
+            <TextInput
+              style={styles.input}
+              keyboardType=""
+              placeholder="Nombre"
+              placeholderTextColor={'#00000'}
+            />
+            <Text style={styles.label}>Correo</Text>
+            <TextInput
+              style={styles.input}
+              keyboardType="phone-pad"
+              placeholderTextColor={'#00000'}
+            />
+            <Text style={styles.label}>Nombre</Text>
+            <TextInput
+              style={styles.input}
+              keyboardType=""
+              placeholderTextColor={'#00000'}
+            />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </Modal>
+  );
 };
+
+const styles = StyleSheet.create({
+    content: {
+        backgroundColor: '#0069a3',
+        flex: 1,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: '600',
+        textAlign: 'center',
+        marginTop: 20,
+        marginEnd: 20,
+        color: '#fff',
+    },
+    boldTitle: {
+        fontWeight: '700',
+    },
+    label: {
+        fontSize: 15,
+        fontWeight: '600',
+        color: '#fff',
+        marginTop: 15,
+        marginBottom:10,
+    },
+    text_input: {
+        marginTop: 40,
+        marginHorizontal: 30,
+        marginBottom:100,
+    },
+    input: {
+        backgroundColor: '#fff',
+        padding: 15,
+        borderRadius: 10,
+        marginBottom: 10,
+    },
+});
